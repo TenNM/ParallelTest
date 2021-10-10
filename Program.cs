5b780d374cfefa29ffe0b1ace8dc65ebda3ee7df
TenNM <pacs3131@gmail.com>
2021-10-10 13:26:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelTest
+{
+    class ParallelInvoke
+    {
+        static void Main()
+        {
+            Console.WriteLine("Method=Main, Thread={0}", Thread.CurrentThread.ManagedThreadId);
+
+            Worker w = new Worker();
+
+            try
+            {
+                var v = Task.Factory.StartNew(
+                    () => w.DoWork()
+                    );
+                //try it instead of Task.Factory.StartNew
+                //Parallel.Invoke(
+                //   () => w.DoWork(continueWork)
+                //);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException.ToString());
+            }
+
+            ConsoleKeyInfo info;
+            do
+            {
+                info = Console.ReadKey(true);
+                switch (info.Key)
+                {
+                    case ConsoleKey.P: w.Pause(); break;
+                    case ConsoleKey.R: w.Resume(); break;
+                }
+            }
+            while (info.Key != ConsoleKey.S);
+            w.Stop();
+
+            Console.WriteLine("Main there");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ParallelTest
+{
+    class Worker
+    {
+        static volatile bool justDoIt = true;
+        static volatile bool pause = false;
+        internal void DoWork()
+        {
+            Console.WriteLine("Method=DoWork, Thread={0}", Thread.CurrentThread.ManagedThreadId);
+            while (justDoIt)
+            {
+                if (pause)
+                {
+                    Thread.Sleep(100);//try to comment it and check u cpu load on Pause();
+                    continue;
+                }
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Work finished");
+        }
+        internal void Pause() => pause = true;
+        internal void Resume() => pause = false;
+        internal void Stop() => justDoIt = false;
+    }
+}
